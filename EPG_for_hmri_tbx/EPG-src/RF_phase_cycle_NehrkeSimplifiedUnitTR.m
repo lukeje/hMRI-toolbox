9d7ff2f6ad1a130521d855cab3b42109785c06b3
ledwards <ledwards@cbs.mpg.de>
2025-03-27 07:39:15 +0100

    Nehrke phase cycling for AFI

---
+++ EPG_for_hmri_tbx/EPG-src/RF_phase_cycle_NehrkeSimplifiedUnitTR.m
@@ -0,0 +1,27 @@
+% Attempt at implementing AFI phase cycling scheme from
+%   Nehrke, K. (2009), On the steady-state properties of actual flip angle imaging (AFI). 
+%   Magn. Reson. Med., 61: 84-92. https://doi.org/10.1002/mrm.21592
+%
+% Simplified by choosing offset so that constant phase offsets in each TR are zero
+% and assuming that TR2 = n*TR1 where n is a positive integer
+function phi = RF_phase_cycle_NehrkeSimplifiedUnitTR(npulse,phi0,n)
+
+phi0 = deg2rad(phi0);
+
+% Different phase increments for even and odd TR
+% Original paper assumes zero-based indexing, so here [1,3,5,...] are "even"
+% and [2,4,6,...] are "odd"
+phi_n = phi0*(n+1)/2; % pseudo-phase increment
+scale_oddeven = [1, n];
+
+increments = [0,0];
+phi = zeros(npulse,1);
+for k=2:npulse
+    % both increments needs to end up scaled by current k, so update them both
+    increments = wrapTo2Pi(increments + phi_n*scale_oddeven);
+
+    oddevenidx = mod(k,2)+1;
+    phi(k) = wrapTo2Pi(phi(k-1) + increments(oddevenidx));
+end
+
+end
